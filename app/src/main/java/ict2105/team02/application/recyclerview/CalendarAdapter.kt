1e00696569befb56ae3c21fab68c1b63db7abd16
maximus-lee-678 <talk_to_max@hotmail.com>
2023-02-17 22:16:57 +0800

    Calendar operational

--- app/src/main/java/ict2105/team02/application/recyclerview/CalendarAdapter.kt
+++ app/src/main/java/ict2105/team02/application/recyclerview/CalendarAdapter.kt
@@ -1,19 +1,40 @@
 package ict2105.team02.application.recyclerview
 
-import android.app.ListActivity
-import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import ict2105.team02.application.R
+import ict2105.team02.application.schedule.DateDetails
 
 class CalendarAdapter(
-    private val size: Int
+    private var dateDetails: DateDetails
 ) : RecyclerView.Adapter<CalendarAdapter.ItemViewHolder>() {
+    var selectedPos: Int = dateDetails.day + dateDetails.firstDayOfMonth - 1
+    var selectedMonthOffset : Int = 0
+
     inner class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.textViewCalendarItem)
+
+        init {
+            textView.setOnClickListener {
+                if (textView.text == "") {
+                    return@setOnClickListener
+                }
+
+                selectedMonthOffset = 0         // reset, user clicked so current month is now active month
+                notifyItemChanged(selectedPos)
+                selectedPos = layoutPosition
+                notifyItemChanged(selectedPos)
+
+                Log.d(
+                    "fish",
+                    textView.text.toString() + "-" + dateDetails.month + "-" + dateDetails.year
+                )
+            }
+        }
     }
 
     /**
@@ -26,7 +47,7 @@ class CalendarAdapter(
         // create a new view
         val adapterLayout = LayoutInflater.from(parent.context)
             .inflate(R.layout.recyclerview_calendar_item, parent, false)
-        adapterLayout.layoutParams.height = (parent.height * 0.16666666).toInt()
+        adapterLayout.layoutParams.height = (parent.height * 0.1).toInt()
 
         return ItemViewHolder(adapterLayout)
     }
@@ -35,12 +56,27 @@ class CalendarAdapter(
      * Replace the contents of a view (invoked by the layout manager)
      */
     override fun onBindViewHolder(holder: CalendarAdapter.ItemViewHolder, position: Int) {
-        holder.textView.text = String.format("#%d", position)
+        // Grid item must be equal to larger than starting day and less than number of days + start day
+        // Position is 0 terminated, must +1 when displaying
+        if (position >= dateDetails.firstDayOfMonth && position < dateDetails.firstDayOfMonth + dateDetails.daysInMonth) {
+            holder.textView.text = String.format("%d", position + 1 - dateDetails.firstDayOfMonth)
+        }
+
+        if (selectedPos == position && selectedMonthOffset == 0) {
+            holder.itemView.setBackgroundColor(0xFF00FF00.toInt())
+        } else {
+            holder.itemView.setBackgroundColor(0xFFFFFFFF.toInt())
+        }
     }
+
 
     /**
      * Return the size of your dataset (invoked by the layout manager)
      */
-    override fun getItemCount() = size
+    override fun getItemCount() = 42
 
+    fun update(dateDetails: DateDetails) {
+        this.dateDetails = dateDetails
+        notifyDataSetChanged()
+    }
 }
