8cd243d4186be97a1e026debb2e8b4efae777c55
maximus-lee-678 <talk_to_max@hotmail.com>
2023-02-19 17:12:59 +0800

    Soft launch of choose layout
    Support for choose layout works fully
    - Access selectedDate array to get clicked dates
    - Selection persists across resets
    
    Known Bugs:
    -Swapping view types wrongly highlights a date
    -Cursed calendar logging

--- app/src/main/java/ict2105/team02/application/recyclerview/CalendarMonthAdapter.kt
+++ app/src/main/java/ict2105/team02/application/recyclerview/CalendarMonthAdapter.kt
@@ -7,34 +7,23 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import ict2105.team02.application.R
+import ict2105.team02.application.schedule.CalendarFragment
 import ict2105.team02.application.schedule.DateDetails
 
 class CalendarMonthAdapter(
     private var dateDetails: DateDetails
 ) : RecyclerView.Adapter<CalendarMonthAdapter.ItemViewHolder>() {
+    private val TAG: String = CalendarFragment::class.simpleName!!
+    private val maxGridCount: Int = 42
+    private val deselectedHex: Int = 0x0F0000FF
+    private val selectedHex: Int = 0x7F00FF00
+
     var selectedPos: Int = dateDetails.day!! + dateDetails.firstDayOfMonth!! - 1
     var selectedMonthOffset : Int = 0
+    var onItemClick: ((IntArray) -> Unit)? = null
 
     inner class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.textViewCalendarMonthItem)
-
-        init {
-            textView.setOnClickListener {
-                if (textView.text == "") {
-                    return@setOnClickListener
-                }
-
-                selectedMonthOffset = 0         // reset, user clicked so current month is now active month
-                notifyItemChanged(selectedPos)
-                selectedPos = layoutPosition
-                notifyItemChanged(selectedPos)
-
-                Log.d(
-                    "fish",
-                    textView.text.toString() + "-" + dateDetails.month + "-" + dateDetails.year
-                )
-            }
-        }
     }
 
     /**
@@ -47,7 +36,7 @@ class CalendarMonthAdapter(
         // create a new view
         val adapterLayout = LayoutInflater.from(parent.context)
             .inflate(R.layout.recyclerview_calendar_month_item, parent, false)
-        adapterLayout.layoutParams.height = (parent.height * 0.08).toInt()
+        adapterLayout.layoutParams.height = (parent.height * 0.07).toInt()
 
         return ItemViewHolder(adapterLayout)
     }
@@ -61,21 +50,41 @@ class CalendarMonthAdapter(
         if (position >= dateDetails.firstDayOfMonth!! && position < dateDetails.firstDayOfMonth!! + dateDetails.daysInMonth!!) {
             holder.textView.text = String.format("%d", position + 1 - dateDetails.firstDayOfMonth!!)
         }
+        else{
+            return
+        }
 
+        // Only highlight day when the user is on the original month
         if (selectedPos == position && selectedMonthOffset == 0) {
-            holder.itemView.setBackgroundColor(0x7F00FF00.toInt())
+            holder.itemView.setBackgroundColor(selectedHex)
         } else {
-            holder.itemView.setBackgroundColor(0x00FFFFFF.toInt())
+            holder.itemView.setBackgroundColor(deselectedHex)
+        }
+
+        // Attach listener that returns date to Fragment and updates selection
+        holder.textView.setOnClickListener {
+            onItemClick?.invoke(intArrayOf(holder.textView.text.toString().toInt(), dateDetails.month!!, dateDetails.year!!))
+
+            selectedMonthOffset = 0  // reset, user clicked so current week is now active week
+            notifyItemChanged(selectedPos)
+            selectedPos = holder.adapterPosition
+            notifyItemChanged(selectedPos)
         }
     }
 
     /**
      * Return the size of your dataset (invoked by the layout manager)
      */
-    override fun getItemCount() = 42
+    override fun getItemCount() = maxGridCount
 
+    /**
+     * Updates stored date details and selected position,
+     * then resets the whole thing.
+     */
     fun update(dateDetails: DateDetails) {
         this.dateDetails = dateDetails
+        selectedPos = dateDetails.day!! + dateDetails.firstDayOfMonth!! - 1
+
         notifyDataSetChanged()
     }
 }
--- app/src/main/java/ict2105/team02/application/recyclerview/CalendarWeekAdapter.kt
+++ app/src/main/java/ict2105/team02/application/recyclerview/CalendarWeekAdapter.kt
@@ -7,13 +7,20 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import ict2105.team02.application.R
+import ict2105.team02.application.schedule.CalendarFragment
 import ict2105.team02.application.schedule.DateDetails
 
 class CalendarWeekAdapter(
     private var dateDetails: DateDetails
 ) : RecyclerView.Adapter<CalendarWeekAdapter.ItemViewHolder>() {
+    private val TAG: String = CalendarFragment::class.simpleName!!
+    private val daysInWeek: Int = 7
+    private val deselectedHex: Int = 0x0F0000FF
+    private val selectedHex: Int = 0x7F00FF00
+
     var selectedPos: Int = dateDetails.day!! - dateDetails.weekArray[0][0]
     var selectedWeekOffset: Int = 0
+    var onItemClick: ((IntArray) -> Unit)? = null
 
     inner class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.textViewCalendarWeekItem)
@@ -38,7 +45,7 @@ class CalendarWeekAdapter(
      */
     override fun onBindViewHolder(holder: CalendarWeekAdapter.ItemViewHolder, position: Int) {
         holder.textView.text = String.format(
-            "[%d] %d-%d-%d",
+            "[pos%d] %d-%d-%d",
             position,
             dateDetails.weekArray[position][0],
             dateDetails.weekArray[position][1],
@@ -46,21 +53,14 @@ class CalendarWeekAdapter(
         )
 
         if (selectedPos == position && selectedWeekOffset == 0) {
-            holder.itemView.setBackgroundColor(0x7F00FF00.toInt())
+            holder.itemView.setBackgroundColor(selectedHex)
         } else {
-            holder.itemView.setBackgroundColor(0x00FFFFFF.toInt())
+            holder.itemView.setBackgroundColor(deselectedHex)
         }
 
+        // Attach listener that returns date to Fragment and updates selection
         holder.textView.setOnClickListener {
-            Log.d(
-                "fish",
-                String.format(
-                    "%d-%d-%d",
-                    dateDetails.weekArray[position][0],
-                    dateDetails.weekArray[position][1],
-                    dateDetails.weekArray[position][2]
-                )
-            )
+            onItemClick?.invoke(dateDetails.weekArray[holder.adapterPosition])
 
             selectedWeekOffset = 0  // reset, user clicked so current week is now active week
             notifyItemChanged(selectedPos)
@@ -72,10 +72,29 @@ class CalendarWeekAdapter(
     /**
      * Return the size of your dataset (invoked by the layout manager)
      */
-    override fun getItemCount() = 7
+    override fun getItemCount() = daysInWeek
 
+    /**
+     * Updates stored date details and selected position,
+     * then resets the whole thing.
+     */
     fun update(dateDetails: DateDetails) {
         this.dateDetails = dateDetails
+
+        // Generate new selected position
+        // First day is in this month: Generate position relative to it
+        // First day is in prev month: Cycle through array find matching day position
+        if (dateDetails.weekArray[0][0]!! < dateDetails.day!!) {
+            selectedPos = dateDetails.day!! - dateDetails.weekArray[0][0]
+        } else {
+            for (i in 0..6) {
+                if (dateDetails.weekArray[i][0] == dateDetails.day!!) {
+                    selectedPos = i
+                    break
+                }
+            }
+        }
+
         notifyDataSetChanged()
     }
 }
