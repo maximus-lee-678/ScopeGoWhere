2213737388b97c1f51610274454002555df03132
lunarbunny <tingwei1734@gmail.com>
2023-02-24 00:53:14 +0800

    Remove unneeded imports

--- app/src/main/java/ict2105/team02/application/viewmodel/EquipmentListViewModel.kt
+++ app/src/main/java/ict2105/team02/application/viewmodel/EquipmentListViewModel.kt
@@ -3,12 +3,9 @@ package ict2105.team02.application.viewmodel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import ict2105.team02.application.utils.Utils
 import ict2105.team02.application.model.Endoscope
 import ict2105.team02.application.repo.DataRepository
 import kotlinx.coroutines.launch
-import okhttp3.*
-import java.io.IOException
 
 class EquipmentListViewModel : ViewModel() {
     val equipments = MutableLiveData<List<Endoscope>>()
