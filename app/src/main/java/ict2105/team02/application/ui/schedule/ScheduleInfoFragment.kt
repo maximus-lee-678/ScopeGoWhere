f3873550610134ba4f8a486db5bf823779dc7f5e
tryhardlaijun <=>
2023-03-17 17:53:50 +0800

    Done changing output from schedule info into eq item

--- app/src/main/java/ict2105/team02/application/ui/schedule/ScheduleInfoFragment.kt
+++ app/src/main/java/ict2105/team02/application/ui/schedule/ScheduleInfoFragment.kt
@@ -1,53 +1,57 @@
 package ict2105.team02.application.ui.schedule
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import ict2105.team02.application.R
 import ict2105.team02.application.databinding.FragmentScheduleInfoBinding
-import ict2105.team02.application.recyclerview.ScheduleAdapter
+import ict2105.team02.application.recyclerview.EquipmentAdapter
+import ict2105.team02.application.ui.dialogs.ScopeDetailFragment
 import ict2105.team02.application.viewmodel.ScheduleInfoViewModel
 
 class ScheduleInfoFragment : Fragment() {
-    private lateinit var adapter: ScheduleAdapter
-    private lateinit var recyclerView: RecyclerView
+    private lateinit var eqAdapter: EquipmentAdapter
     private lateinit var binding: FragmentScheduleInfoBinding
-    //    private val sharedViewModel: WashViewModel by activityViewModels()
     private lateinit var viewModel: ScheduleInfoViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentScheduleInfoBinding.inflate(inflater)
         viewModel = ViewModelProvider(requireActivity()).get(ScheduleInfoViewModel::class.java)
 
+        binding.scheduleRecycle.setHasFixedSize(false)
+        eqAdapter = EquipmentAdapter()
+
+        binding.scheduleRecycle.apply {
+            this.layoutManager = LinearLayoutManager(requireContext())
+            this.adapter = eqAdapter
+        }
+
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        // Set onItemClick before observing the LiveData
+        eqAdapter.onItemClick = {
+            val fragment = ScopeDetailFragment.newInstance(it.scopeSerial)
+            Log.d("EquipmentAdapter", "Setup OnClick")
+            fragment.show(requireActivity().supportFragmentManager, "scope_detail")
+        }
+
+
         viewModel.fetchAllScheduledScope() {
             activity?.runOnUiThread {
                 binding.loadEquipmentProgressIndicator.visibility = View.INVISIBLE
             }
         }
 
-        val layoutManager = LinearLayoutManager(context)
-        // Find recycler view
-        recyclerView = view.findViewById(R.id.scheduleRecycle)
-        // Set layout
-        recyclerView.layoutManager = layoutManager
-        recyclerView.setHasFixedSize(false)
-        adapter = ScheduleAdapter(ArrayList())
-        recyclerView.adapter = adapter
-        viewModel.getSchedule()?.observe(viewLifecycleOwner) {
-            adapter.submitList(it)
+        viewModel.getScheduledEndoscope()?.observe(viewLifecycleOwner) {
+            eqAdapter.submitList(it)
         }
-
-
     }
-
 }
